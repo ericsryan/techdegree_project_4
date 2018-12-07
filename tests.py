8ab9238b474b26d1abe1d18ba3a94130e9c8736d
Eric S. Ryan <eric@ericsryan.com>
2018-12-07 13:31:15 -0800

    Merge 2

--- tests.py
+++ tests.py
@@ -2,30 +2,16 @@ import datetime
 import mock
 import unittest
 
-from utils import get_date
-from utils import login
-from utils import nav_bar
+from work_log import get_date
 
 
 class UtilsTests(unittest.TestCase):
     def setUp(self):
         pass
 
-
     def test_get_date(self):
-        # Normal testcase
-        user_input = ['13-11-2011', '11-11-2011']
-        with patch('utils.input', side_effect=user_input):
-            expected_date = datetime.datetime(2011, 11, 11, 0, 0)
-            actual_date = get_date("> ")
-            self.assertEqual(actual_date, expected_date)
-
-
-    def test_nav_bar(self):
-        expected_nav_bar = '[P]revious | [N]ext | [E]dit | [D]elete | [S]earch Menu'
-        actual_nav_bar = nav_bar('pneds')
-        self.assertEqual(actual_nav_bar, expected_nav_bar)
-
+        self.assertEqual(get_date("> "),
+                         datetime.datetime(2011, 11, 11, 0, 0))
 
 
 if __name__ == '__main__':
